b581e61f5071a918e290f27562889a13336a28fa
Miguel P <spearditi@gmail.com>
2016-02-20 11:12:45 -0600

    Added networked player script to networked player object

--- Drawcula/Assets/TeamScripts/NetworkedPlayer.cs
+++ Drawcula/Assets/TeamScripts/NetworkedPlayer.cs
@@ -9,17 +9,4 @@ public class NetworkedPlayer : MonoBehaviour {
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings ("0.1");
 	}
-
-	//Join online cloud lobby and specified room
-	void OnJoinedLobby() {
-		Debug.Log ("joined lobby");
-
-		RoomOptions roomOptions = new RoomOptions() {};
-		PhotonNetwork.JoinOrCreateRoom (_room, roomOptions, TypedLobby.Default);
-	}
-
-	//After connecting to room in lobby, instantiate the player
-	void OnJoinedRoom() {
-		//PhotonNetwork.Instantiate ("BGPlayerController", Vector3.zero, Quaternion.identity, 0);
-	}
 }
