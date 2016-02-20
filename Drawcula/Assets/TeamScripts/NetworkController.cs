d04b1557ab249bc232320e99e8a2633c5609fca1
Miguel P <spearditi@gmail.com>
2016-02-20 11:01:37 -0600

    Added script that connects project to photon server

--- Drawcula/Assets/TeamScripts/NetworkController.cs
+++ Drawcula/Assets/TeamScripts/NetworkController.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class NetworkController : MonoBehaviour {
 
-	// Use this for initialization
+	string _room = "DrawculaTest";
+
+	//On startup connect to photon network 
 	void Start () {
-	
+		Debug.Log ("Connecting to Photon Server for Drawcula Project");
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+		Debug.Log ("Connection to Photon Server successful!");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	//Join online cloud lobby and specified room
+	void OnJoinedLobby() {
+		Debug.Log ("Joined Lobby for Drawcula Project");
+		RoomOptions roomOptions = new RoomOptions() {};
+		PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+	}
+
+	//After connecting to room in lobby, instantiate the player
+	void OnJoinedRoom() {
+		//PhotonNetwork.Instantiate ("BGPlayerController", Vector3.zero, Quaternion.identity, 0);
 	}
 }
--- Drawcula/Assets/TeamScripts/NetworkedPlayer.cs
+++ Drawcula/Assets/TeamScripts/NetworkedPlayer.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class NetworkedPlayer : MonoBehaviour {
 
-	// Use this for initialization
+	string _room = "DrawculaTest";
+
+	//On startup connect to photon network 
 	void Start () {
-	
+		PhotonNetwork.ConnectUsingSettings ("0.1");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	//Join online cloud lobby and specified room
+	void OnJoinedLobby() {
+		Debug.Log ("joined lobby");
+
+		RoomOptions roomOptions = new RoomOptions() {};
+		PhotonNetwork.JoinOrCreateRoom (_room, roomOptions, TypedLobby.Default);
+	}
+
+	//After connecting to room in lobby, instantiate the player
+	void OnJoinedRoom() {
+		//PhotonNetwork.Instantiate ("BGPlayerController", Vector3.zero, Quaternion.identity, 0);
 	}
 }
