f3e806788d7ea96ed65a632b4de7e6e3b20d352f
Miguel P <spearditi@gmail.com>
2016-02-20 11:54:53 -0600

    Updated networked player scripts and prefabs to associate them respectively

--- Drawcula/Assets/TeamScripts/NetworkedPlayer.cs
+++ Drawcula/Assets/TeamScripts/NetworkedPlayer.cs
@@ -1,13 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkedPlayer : MonoBehaviour {
+public class NetworkedPlayer : Photon.MonoBehaviour {
+
+	public GameObject avatar;
+	public Transform playerGlobal;	//where the avatar is in space
+	public Transform playerLocal;	//avatar head movements
 
 	void Start () {
 		Debug.Log ("I'm instantiated");
+
+		//only if we're the player
+		if(photonView.isMine)
+		{
+			playerGlobal = GameObject.Find("MainPlayerMan").transform;
+			playerLocal = playerGlobal.Find ("Camera");
+
+			//associate camera with avatar
+			this.transform.SetParent(playerLocal);
+		}
 	}
 
-	void Update() {
-		
+	//Get new data and send to all players in room
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		//our player sending information out to players
+		if (stream.isWriting)
+		{
+			stream.SendNext(playerGlobal.position);
+			stream.SendNext(playerGlobal.rotation);
+			stream.SendNext(playerLocal.localPosition);
+			stream.SendNext(playerLocal.localRotation);
+		}
+		else    //our player recieving information from other players
+		{
+			this.transform.position = (Vector3)stream.ReceiveNext();
+			this.transform.rotation = (Quaternion)stream.ReceiveNext();
+			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+		}
 	}
 }
